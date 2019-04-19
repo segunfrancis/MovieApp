869b8fcdc66ca8585b09f8aea1640654b86f8b0d
segunfrancis <francis4dblues@gmail.com>
2019-04-19 13:55:26 +0100

    Add TrailerResponse.java class

--- app/src/main/java/com/example/computer/moviesearch/model/TrailerResponse.java
+++ app/src/main/java/com/example/computer/moviesearch/model/TrailerResponse.java
@@ -1,4 +1,30 @@
 package com.example.computer.moviesearch.model;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 public class TrailerResponse {
+
+    @SerializedName("id")
+    private int id_trailer;
+
+    @SerializedName("results")
+    private List<Trailer> results;
+
+    public int getId_trailer() {
+        return id_trailer;
+    }
+
+    public void setId_trailer(int id_trailer) {
+        this.id_trailer = id_trailer;
+    }
+
+    public List<Trailer> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Trailer> results) {
+        this.results = results;
+    }
 }
